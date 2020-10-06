d2a28c1e2397f281ad8a11442251d0b4e15253a8
ffd <ffdelacruz@icloud.com>
2020-10-07 01:37:01 +0800

    LoggedOutInteractor: Implement login method defined in LoggedOutPresentableListener

--- TicTacToe/LoggedOut/LoggedOutInteractor.swift
+++ TicTacToe/LoggedOut/LoggedOutInteractor.swift
@@ -6,25 +6,35 @@ import RIBs
 import RxSwift
 
 protocol LoggedOutRouting: ViewableRouting {
-    // TODO: Declare methods the interactor can invoke to manage sub-tree via the router.
+    
 }
 
 protocol LoggedOutPresentable: Presentable {
     var listener: LoggedOutPresentableListener? { get set }
-    // TODO: Declare methods the interactor can invoke the presenter to present data.
 }
 
-protocol LoggedOutListener: class {
-    // TODO: Declare methods the interactor can invoke to communicate with other RIBs.
+protocol LoggedOutListener: AnyObject {
+    
 }
 
 final class LoggedOutInteractor: PresentableInteractor<LoggedOutPresentable>, LoggedOutInteractable, LoggedOutPresentableListener {
+    func login(withPlayer1Name player1Name: String?, player2Name: String?) {
+        let player1NameWithDefault = playerName(player1Name, withDefaultName: "Player 1")
+        let player2NameWithDefault = playerName(player2Name, withDefaultName: "Player 2")
+        print("\(player1NameWithDefault) vs \(player2NameWithDefault)")
+    }
+    
+    private func playerName(_ name: String?, withDefaultName defaultName: String) -> String {
+        if let name = name {
+            return name.isEmpty ? defaultName : name
+        } else {
+            return defaultName
+        }
+    }
 
     weak var router: LoggedOutRouting?
     weak var listener: LoggedOutListener?
 
-    // TODO: Add additional dependencies to constructor. Do not perform any logic
-    // in constructor.
     override init(presenter: LoggedOutPresentable) {
         super.init(presenter: presenter)
         presenter.listener = self
@@ -32,11 +42,9 @@ final class LoggedOutInteractor: PresentableInteractor<LoggedOutPresentable>, Lo
 
     override func didBecomeActive() {
         super.didBecomeActive()
-        // TODO: Implement business logic here.
     }
 
     override func willResignActive() {
         super.willResignActive()
-        // TODO: Pause any business logic.
     }
 }
