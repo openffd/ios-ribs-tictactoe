36bbc29113c8fff7146ae0563181707f75f69bca
ffd <ffdelacruz@icloud.com>
2020-10-07 21:49:54 +0800

    LoggedOutInteractor: Call listener's didLogin(::) upon login

--- TicTacToe/LoggedOut/LoggedOutInteractor.swift
+++ TicTacToe/LoggedOut/LoggedOutInteractor.swift
@@ -21,7 +21,7 @@ final class LoggedOutInteractor: PresentableInteractor<LoggedOutPresentable>, Lo
     func login(withPlayer1Name player1Name: String?, player2Name: String?) {
         let player1NameWithDefault = playerName(player1Name, withDefaultName: "Player 1")
         let player2NameWithDefault = playerName(player2Name, withDefaultName: "Player 2")
-        print("\(player1NameWithDefault) vs \(player2NameWithDefault)")
+        listener?.didLogin(withPlayer1Name: player1NameWithDefault, player2Name: player2NameWithDefault)
     }
     
     private func playerName(_ name: String?, withDefaultName defaultName: String) -> String {
